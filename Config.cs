022e1f4c2bca8ce33bd8c64f054ff0794b9fb9c9
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2022-06-30 15:47:53 +0400

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,36 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackOut
+{
+    public class Config : IConfig
+    {
+        [Description("↓Indicates whether the plugin is enabled or not")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("↓Indicates whether the subtitles for cassie will be shown or not")]
+        public bool Subtitles { get; set; } = true;
+        [Description("↓Specifies which cassie will be used when the light is turned off")]
+        public string CassieLight { get; set; } = "jam_70_5 warning .g5 warning ChaosInsurgency Hacked Complex jam_70_5 system . . Return system";
+        [Description("↓Indicates whether cassie will be played when the lights are turned off. (false = will not. true = will be)")]
+        public bool offCassie { get; set; } = true;
+
+        [Description("Specifies in which seconds a random time will be selected\n" +
+            "↓Minimum value")]
+        public int MinBlackoutStartTime { get; set; } = 60;
+        [Description("↓Maximum value")]
+        public int MaxBlackoutStartTime { get; set; } = 300;
+
+        [Description("Indicates how many seconds the light is turned off\n" +
+            "↓Minimum value")]
+        public int MinBlackoutTime { get; set; } = 30;
+        [Description("↓Maximum value")]
+        public int MaxBlackoutTime { get; set; } = 30;
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,70 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using MEC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackOut
+{
+    class Plugin : Plugin<Config>
+    {
+        public override string Prefix => "BlackOut";
+        public override string Name => "BlackOut";
+        public override string Author => "Rysik5318";
+
+        public static Plugin plugin;
+        public override Version Version { get; } = new Version(1, 1, 7);
+
+        bool BlackoutOFF = false;
+
+        bool RoundStart = false;
+
+        public override void OnEnabled()
+        {
+            plugin = this;
+            base.OnEnabled();
+            Exiled.Events.Handlers.Server.RoundStarted += OnRoundStart;
+        }
+
+        public override void OnDisabled()
+        {
+            plugin = null;
+            Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStart;
+            base.OnDisabled();
+        }
+
+        private void OnRoundStart()
+        {
+            Random rnd = new Random();
+            BlackoutOFF = false;
+            RoundStart = true;
+            Timing.CallDelayed(rnd.Next(Config.MinBlackoutStartTime, Config.MaxBlackoutStartTime), () => Blackout());
+        }
+        private void Blackout()
+        {
+            if (RoundStart)
+            {
+                if (!BlackoutOFF)
+                {
+                        IEnumerable<ZoneType> zoneTypes = new List<ZoneType>() { ZoneType.Entrance, ZoneType.HeavyContainment, ZoneType.LightContainment };
+                        Random rnd = new Random();
+                        float time = rnd.Next(Config.MinBlackoutTime, Config.MaxBlackoutTime);
+                        Map.TurnOffAllLights(time, ZoneType.Entrance);
+                        Map.TurnOffAllLights(time, ZoneType.HeavyContainment);
+                        Map.TurnOffAllLights(time, ZoneType.LightContainment);
+                        if (Config.offCassie)
+                        {
+                            Timing.CallDelayed(3f, () => Cassie.Message(Config.CassieLight, false, true, Config.Subtitles));
+                        }
+                        else if (!Config.offCassie);
+                        Timing.CallDelayed(rnd.Next(Config.MinBlackoutStartTime, Config.MaxBlackoutStartTime), () => Blackout());
+                        BlackoutOFF = true;
+                }
+            }
+        }
+    }
+}
